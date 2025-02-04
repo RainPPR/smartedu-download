3831db0c0dd4aaf672bdbb4cee2057c13dac2022
RainPPR <41764766+RainPPR@users.noreply.github.com>
2025-02-04 18:10:43 +0800

    Update main.py

--- script/main.py
+++ script/main.py
@@ -1,11 +1,21 @@
 import sys
 import download
+import threading
 
 def Main():
     if len(sys.argv) == 1:
         raise ValueError("传入的参数格式不正确")
+
+    threads = []
     for id in sys.argv[1:]:
-        download.download(id, '../pdf/')
+        thread = threading.Thread(target=download.download, args=(id, '../pdf/'))
+        threads.append(thread)
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+    print("所有下载完成！")
 
 if __name__ == "__main__":
     Main()
